35423e508518768bf97a0558a6d25ef4d5cf7785
abcoinseo <abcoinseo@gmail.com>
2026-02-15 21:38:53 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 if [ ! -d "/app/data" ]; then
-  mkdir /app/data
+  mkdir -p /app/data
 fi
 
-if [ ! -f "/app/data/genesis.json" ]; then
-  polygon-edge genesis --chain-id 18591223 --name "VELOX Chain" --premine 0xD7e39d8bbE6FA5eE59Ff8a945D93E16deaC56210:1000000000000000000000000000
+if [ ! -f "/app/data/velox-genesis.json" ]; then
+  cp genesis.json /app/data/velox-genesis.json
 fi
 
 polygon-edge server \
   --data-dir /app/data \
-  --chain genesis.json \
+  --chain /app/data/velox-genesis.json \
   --libp2p 0.0.0.0:10000 \
   --jsonrpc 0.0.0.0:8545
